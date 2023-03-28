237c7a9046686569adee2a4f3a1e28cd586881b7
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-03-28 16:22:53 -0300

    Implementação do React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Container from './components/layout/Container'
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Costs</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/contact'>Contato</Link>
+                <Link to='/company'>Empresa</Link>
+                <Link to='/newproject'>Novo Projeto</Link>
+            </div>
+            <Container customClass="min-height">
+                    <Routes>
+                        <Route exact path='/' element={<Home />} />
+                        <Route exact path='/company' element={<Company />} />
+                        <Route exact path='/contact' element={<Contact />} />
+                        <Route exact path='/newproject' element={<NewProject />} />
+                    </Routes>
+            </Container>
+            <p>Footer</p>
+        </Router>
+    )
 }
 
-export default App;
+export default App
+
